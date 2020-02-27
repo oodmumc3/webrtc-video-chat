d6ba946b6bc23901253de9100f81b7569013b158
jin.bak <jin.bak@kakaocorp.com>
2020-02-27 20:21:49 +0900

    add stun and turn server

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,15 @@
 navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
 window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-window.RTCIceCandidate = window.RTCIceCandidate || window.mozRTCIceCandidate || window.webkitRTCIceCandidate;
 window.RTCSessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription || window.webkitRTCSessionDescription;
 
 var isAlreadyCalling = false;
-var peerConnection = new RTCPeerConnection();
+var peerConnection = new RTCPeerConnection({
+    iceServers: [
+        {
+            'urls': 'stun:stun.l.google.com:19302'
+        }
+    ]
+});
 
 
 peerConnection.onaddstream = function (obj) {
